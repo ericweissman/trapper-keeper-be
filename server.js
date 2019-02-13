4029926a7e5954a51298412049482cfa6344a3c5
Travis Gee <geet084@gmail.com>
2019-02-13 09:57:37 -0700

    Update notes to return items as well

--- server.js
+++ server.js
@@ -10,19 +10,19 @@ app.listen(app.get('port'), () => {
 
 app.locals.title = 'Trapper Keeper';
 app.locals.notes = [{ id: 1550017862941, title:"Eric is gone"}];
-app.locals.items = [];
+app.locals.items = [{ description: 'sup and stuff', noteID: 1550017862941, id: 6 }];
 
 //NOTES
 //Get all notes
 app.get('/api/v1/notes', (request, response) => {
   const notes = app.locals.notes;
-  return response.json({notes})
+  const items = app.locals.items;
+  return response.json({notes, items})
 });
 
 //add a new note
 app.post('/api/v1/notes', (request, response) => {
-  // const id = Date.now();
-  const { id, title } = request.body;
+  const { id, title, items } = request.body;
 
   if (!title) {
     return response.status(422).send({
@@ -30,7 +30,8 @@ app.post('/api/v1/notes', (request, response) => {
     });
   } else {
     app.locals.notes.push({ id, title })
-    return response.status(201).json( { id, title });
+    app.locals.items.push(...app.locals.items, ...items)
+    return response.status(201).json({ id, title, items });
   }
 });
 
@@ -39,8 +40,10 @@ app.post('/api/v1/notes', (request, response) => {
 app.get('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
   const note = app.locals.notes.find(note => note.id == id);
+  const items = app.locals.items.filter(item => item.noteID == id)
+
   if (note) {
-    return response.status(200).json(note)
+    return response.status(200).json({note, items})
   } else {
     return response.sendStatus(404)
   }
@@ -53,7 +56,7 @@ app.delete('/api/v1/notes/:id', (request, response) => {
     return note.id != id
   })
   app.locals.notes = updatedNotes
-  response.status(202).json(`Note ${id} has been deleted`)
+  response.status(202).json(`Note ${id} has been deleted successfully`)
 });
 
 //edit a specific note
@@ -66,7 +69,7 @@ app.put('/api/v1/notes/:id', (request, response) => {
       return note
   })
   app.locals.notes = updatedNotes
-  response.status(202).json(`${request.body.title} has been updated`)
+  response.status(202).json(`Note ${id} has been updated to: ${request.body.title}`)
 });
 
 //ITEMS
