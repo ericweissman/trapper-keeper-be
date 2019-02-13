3809c9d95cd0ca51a52360d3216b241cc1804a19
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-13 13:52:00 -0700

    Refactor to manipulate items in notes

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.listen(app.get('port'), () => {
 })
 
 app.locals.title = 'Trapper Keeper';
-app.locals.notes = [{ id: 1550017862941, title:"Eric is gone"}];
-app.locals.items = [{ description: 'sup and stuff', noteID: 1550017862941, id: 6 }];
+app.locals.notes = [{ id: 1550017862941, title: "Eric is gone" }, { id: 15500222, title: "Matt is here" }];
+app.locals.items = [{ description: 'sup and stuff', noteID: 1550017862941, id: 6 }, { description: 'new2', noteID: 1550017862941, id: 7 }, { description: 'new3', noteID: 1550017862941, id: 8 },
+  { description: 'sup aff', noteID: 15500222, id: 2 }, { description: '2222', noteID: 15500222, id: 3 }];
 
 //NOTES
 //Get all notes
@@ -52,74 +53,28 @@ app.get('/api/v1/notes/:id', (request, response) => {
 //delete a specific note
 app.delete('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
-  const updatedNotes = app.locals.notes.filter((note) => {
-    return note.id != id
-  })
+  const updatedNotes = app.locals.notes.filter(note => note.id != id)
+  const updatedItems = app.locals.items.filter(item => item.noteID != id)
   app.locals.notes = updatedNotes
+  app.locals.items = updatedItems
   response.status(202).json(`Note ${id} has been deleted successfully`)
 });
 
 //edit a specific note
 app.put('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
+  const { title, items} = request.body
+
   const updatedNotes = app.locals.notes.map((note) => {
     if (note.id == id) {
-      note.title = request.body.title
+      note.title = title
     } 
-      return note
+      return note 
   })
   app.locals.notes = updatedNotes
-  response.status(202).json(`Note ${id} has been updated to: ${request.body.title}`)
-});
 
-//ITEMS
-app.get('/api/v1/items', (request, response) => {
-  const items = app.locals.items;
-  return response.json({ items })
-});
+  const cleanedItems = app.locals.items.filter(item => item.noteID != id)
+  app.locals.items = [...cleanedItems, ...items]
 
-app.post('/api/v1/items', (request, response) => {
-  const id = Date.now();
-
-  const { description, isComplete, noteID } = request.body;
-
-  if (!title) {
-    return response.status(422).send({
-      error: 'No note title provided'
-    });
-  } else {
-    app.locals.notes.push({ id, title })
-    return response.status(201).json({ id, title });
-  }
-});
-
-app.get('/api/v1/notes/:id', (request, response) => {
-  const { id } = request.params;
-  const note = app.locals.notes.find(note => note.id == id);
-  if (note) {
-    return response.status(200).json(note)
-  } else {
-    return response.sendStatus(404)
-  }
-});
-
-app.delete('/api/v1/notes/:id', (request, response) => {
-  const { id } = request.params;
-  const updatedNotes = app.locals.notes.filter((note) => {
-    return note.id != id
-  })
-  app.locals.notes = updatedNotes
-  response.status(202).json(`Note ${id} has been removed`)
-});
-
-app.put('/api/v1/notes/:id', (request, response) => {
-  const { id } = request.params;
-  const updatedNotes = app.locals.notes.map((note) => {
-    if (note.id == id) {
-      note.title = request.body.title
-    }
-    return note
-  })
-  app.locals.notes = updatedNotes
-  response.status(202).json(`${request.body.title} has been updated`)
+  response.status(202).json(`Note ${id} has been updated to: ${title}`)
 });
