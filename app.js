feebb5da34bffd5779f8295042768a77bd7d6039
Matthew Foxwell <foxwellmw@gmail.com>
2019-02-18 09:56:40 -0700

    Remove dummy data for testing

--- app.js
+++ app.js
@@ -5,19 +5,16 @@ app.use(cors())
 app.use(express.json())
 
 app.locals.title = 'Trapper Keeper';
-app.locals.notes = [{ id: 1550017862941, title: "Eric is gone" }, { id: 15500222, title: "Matt is here" }];
-app.locals.items = [{ description: 'sup and stuff', noteID: 1550017862941, id: 6 }, { description: 'new2', noteID: 1550017862941, id: 7 }, { description: 'new3', noteID: 1550017862941, id: 8 },
-{ description: 'sup aff', noteID: 15500222, id: 2 }, { description: '2222', noteID: 15500222, id: 3 }];
+app.locals.notes = [];
+app.locals.items = [];
 
-//NOTES
-//Get all notes
 app.get('/api/v1/notes', (request, response) => {
   const notes = app.locals.notes;
   const items = app.locals.items;
   return response.status(200).json({ notes, items })
 });
 
-//add a new note
+//Add a new note
 app.post('/api/v1/notes', (request, response) => {
   const { id, title, items } = request.body;
 
@@ -30,8 +27,7 @@ app.post('/api/v1/notes', (request, response) => {
   }
 });
 
-
-//get a specific note
+//Get a specific note
 app.get('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
   const note = app.locals.notes.find(note => note.id == id);
@@ -44,7 +40,7 @@ app.get('/api/v1/notes/:id', (request, response) => {
   }
 });
 
-//delete a specific note
+//Delete a specific note
 app.delete('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
   const updatedNotes = app.locals.notes.filter(note => note.id != id)
@@ -59,27 +55,26 @@ app.delete('/api/v1/notes/:id', (request, response) => {
   }
 });
 
-//edit a specific note
+//Edit a specific note
 app.put('/api/v1/notes/:id', (request, response) => {
   const { id } = request.params;
   const { title, items } = request.body
   const note = app.locals.notes.find(note => note.id == id);
-  if(note) {
-    const updatedNotes = app.locals.notes.map((note) => {
+  
+  if (note) {
+    const updatedNotes = app.locals.notes.map(note => {
       if (note.id == id) {
         note.title = title
       }
       return note
     })
     app.locals.notes = updatedNotes
-
     const cleanedItems = app.locals.items.filter(item => item.noteID != id)
     app.locals.items = [...cleanedItems, ...items]
     return response.status(202).json(`Note ${id} has been updated`)
   } else {
     return response.status(404).json('That note does not exist, nothing was edited')
   }
-  
 });
 
 export default app
